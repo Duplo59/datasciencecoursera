d0b19b0a6f86b0f352d93eb872156745e015d66d
Duplo59 <p.guderzo@gmail.com>
2018-09-02 23:25:04 +0200

    Secondo esercizio

---
+++ Course_04-Exploratory_Data_Analysis/Course_Project_1/plot2.R
@@ -0,0 +1,40 @@
+# ------------------------------------------------------------------------------
+#
+# plot2.R. Second graph to do
+#
+# ------------------------------------------------------------------------------
+# Set variables and load packages
+# ------------------------------------------------------------------------------
+library(plyr)
+library(dplyr)
+# Save working directory
+currentPath <- getwd()
+# Save input file names
+inputFile <- "household_power_consumption.txt"
+
+# ------------------------------------------------------------------------------
+# Read file with specification of NA field and with no factor variable. Then
+# change two fields format (otherwise they are in character format). Subset
+# dataset
+# ------------------------------------------------------------------------------
+# Read txt file 
+hpc_data <- read.table(file.path(currentPath, inputFile), header = TRUE, 
+sep = ";", na.strings="?", stringsAsFactors=FALSE)
+# Change Date from character format to Time format
+# hpc_data <- mutate(hpc_data, Date = as.Date(Date, "%d/%m/%Y"))
+# Change format Global_active_power da character a numeric
+hpc_data <- mutate(hpc_data, Global_active_power = as.numeric(Global_active_power))
+# Create/add a unique date/time variable
+hpc_data <- mutate(hpc_data, dateTime = as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S"))
+# Filter only the required information by Date range. NB: the standard date format
+# is yyyy-mm-dd
+hpc_data <- filter(hpc_data, (dateTime >= "2007-02-01 00:00:00") & (dateTime <= "2007-02-02 23:59:00"))
+# ------------------------------------------------------------------------------
+# Plot graph with the desired dimensions
+# ------------------------------------------------------------------------------
+# Initialize png plot with required width and height
+png("plot2.png", width=480, height=480)
+# Plot the graph. Type 1 forces a line plot.
+plot(hpc_data$dateTime, hpc_data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
+
