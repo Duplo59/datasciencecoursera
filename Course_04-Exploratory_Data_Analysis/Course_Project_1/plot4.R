b4a6afdc2bdbcfe08b5d1ca0c088d7fbaa47fb66
Duplo59 <p.guderzo@gmail.com>
2018-09-03 12:09:38 +0200

    update graph 2 parameters

--- Course_04-Exploratory_Data_Analysis/Course_Project_1/plot4.R
+++ Course_04-Exploratory_Data_Analysis/Course_Project_1/plot4.R
@@ -56,7 +56,7 @@ with (hpc_data, lines(dateTime, Sub_metering_2, type="l", col="red"))
 with (hpc_data, lines(dateTime, Sub_metering_3, type="l", col="blue"))
 # Apply legend
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-col=c("black", "red", "blue"), lty=c(1,1), lwd=c(1,1))
+col=c("black", "red", "blue"), lty= c(1, 1), bty="n", cex = 0.95)
 
 
 # ------------------------------------------------------------------------------
